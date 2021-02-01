5dceff4bc685120970457d13957f72064a23bd4d
Nikolas S. Logemann <nikolasl@outlook.de>
2021-02-01 12:00:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,12 +10,14 @@ main (int argc, char **argv)
 		return 1;
 	}
 
+	// Get mode from ocal
 	mode_t mode;
 	if (sscanf(argv[1], "%o", &mode) != 1)
 		return 1;
 
+	// Try to change mode
 	if (chmod(argv[2], mode) < 0)
 		return 1;
-
+	
 	return 0;
 }
